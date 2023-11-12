094e89dacc078840301aa39cc3871971aa4ea370
Divyanshu Tripathi <104244101+Divyanshu-2022@users.noreply.github.com>
2023-11-12 21:25:09 +0530

    tgvtd
    dgvbdg

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const audio = document.getElementById('backgroundAudio');
     const playButton = document.getElementById('playButton');
 
-    playButton.addEventListener('click', function() {
+    // Function to play audio
+    function playAudio() {
         audio.play()
             .then(() => {
                 console.log('Audio is playing');
@@ -10,5 +11,23 @@ document.addEventListener('DOMContentLoaded', function() {
             .catch(error => {
                 console.log('Error playing audio:', error);
             });
+    }
+
+    // Trigger playAudio function on window load
+    window.onload = function() {
+        playAudio();
+    };
+
+    // Fallback for browsers that may need a user interaction
+    document.addEventListener('click', function() {
+        playAudio();
     });
+
+    // Fallback for mobile devices
+    document.addEventListener('touchstart', function() {
+        playAudio();
+    });
+
+    // Trigger a fake click event on the invisible button
+    playButton.click();
 });
